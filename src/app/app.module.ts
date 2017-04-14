b60694a4be5e4a46a457dba27172afecee1b9549
Nestor Maysonet <audacious.anarchist@gmail.com>
2017-04-13 21:10:40 -0400

    Added /home route, routing module, added declarations in app.module, added downloads folder and stubs for html and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+//Imports needed for angular functionality
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule} from '@angular/material';
 
+//Need hammerjs as dependency for some aspects of angular-material
+import 'hammerjs';
+
+//Components, Modules, and Services imports
 import { AppComponent } from './app.component';
+import { HomeComponent} from './home/home.component';
+import { AppRoutingModule} from './routes/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
