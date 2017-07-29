23799356baf4cb8b7c3f230f96702f2a82003def
Snowphones <DrewDignan@gmail.com>
2017-07-28 23:37:57 -0400

    Added comments and cleaned up code

--- server/routes/api.js
+++ server/routes/api.js
@@ -43,11 +43,13 @@ router.get('/user', (req, res) => {
 	}
 });
 
+//gets userid for the game. can check via email or username
 router.get('/gameuser', (req, res) =>{
 
 	var userData;
 	var inQuery;
 
+	//sees if username and/or email are defined. If one is query on it, otherwise err.
 	if(req.query.username == undefined && req.query.email == undefined)
 	{
 		res.send('ERR: NO PARAMETERS TO SEARCH FOR');
@@ -63,11 +65,13 @@ router.get('/gameuser', (req, res) =>{
 		inQuery = "SELECT USERID FROM USERS WHERE EMAIL_ADDR = ?";
 	}
 
+	//query db for the userid. returns an integer.
 	dbConn.queryDB(mysql.format(inQuery, userData), function(val, err){
 		if(err) {
 			res.send(null);
 		}
 		else {
+			//converts the query result into an int
 			res.send(JSON.stringify(val[0].USERID));
 		}
 		
@@ -135,40 +139,41 @@ router.get('/userinfo', (req, res) => {
 });
 
 //http://localhost:3000/api/multiuser?userid=["1","4","8"]
+//gets the data from multiple users. Array should be defined as above. Will ignore any id's that don't have users yet.
 router.get('/multiuser', (req, res) => {
 
+	//verifies that it was sent data
 	if(req.query.userid == undefined)
 	{
 		res.send('ERROR: NO USERID PARAMETER');
 	}
 	else
 	{
-		var inQuery = 'SELECT * FROM USERS WHERE USERID = ';
+		//sets up query
+		var inQuery = 'SELECT * FROM USERS WHERE USERID = ?';
+		//converts the array of id's into an actual array for JS.
 		var arr = JSON.parse(req.query.userid);
 
+		//if we were sent data, build a query that searches for each index.
 		if(arr.length > 1)
 		{
-			var i = 0;
-			for(i = 0; i < arr.length; i++)
+			var i;
+
+			for(i = 1; i < arr.length; i++)
 			{
-				inQuery += arr[i];
 				if(i+1 < arr.length)
 				{
-					inQuery += ' OR USERID = ';
+					inQuery += ' OR USERID = ?';
 				}
 			}
-		}
-		else if(arr.length == 1)
-		{
-			inQuery = 'SELECT * FROM USERS WHERE USERID = ';
-			inQuery = mysql.format(inQuery, arr[0]);
 		}
 		else
 		{
 			res.send('ERROR: NO USERS SELECTED');
 		}
 
-		dbConn.queryDB(inQuery, function(val, err){
+		//query on the data by inserting the arr into the query
+		dbConn.queryDB(mysql.format(inQuery, arr), function(val, err){
 			if(err) {
 				res.send(val);
 			}
@@ -180,20 +185,23 @@ router.get('/multiuser', (req, res) => {
 	}
 });
 
+//gets all the info from a scenario row with scenarioid = inID
 router.get('/scenario', (req, res) => {
 	
-	//base query
+	//gets the scenarioid that was passed.
 	var scenarioid = req.query.scenid;
 
+	//verifies we were sent data
 	if(scenarioid == undefined)
 	{
 		res.send('ERROR: NO SCENARIOID PARAMETER');
 	}
 	else
 	{
+		//query
 		var inQuery = "SELECT * FROM SCENARIOS WHERE SCENARIOID = ?";
 
-		//searches db for user with userid = id. 
+		//searches db for scenarios with scenarioid = scenid
 		dbConn.queryDB(mysql.format(inQuery, scenarioid), function(val, err){
 			//if none was found return err message, otherwise JSONify data and return
 			if(err) {
@@ -206,112 +214,145 @@ router.get('/scenario', (req, res) => {
 	}
 });
 
+//gets the most recent scores of all users who played a scenario
 router.get('/scenarioscores', (req, res) => {
 
+	//gets the scenarioid that was passed
 	var scenarioid = req.query.scenid;
 
+	//verifies that it was passes the parameters
 	if(scenarioid == undefined)
 	{
 		res.send('ERROR: NO SCENARIOID PARAMETER')
 	}
 	else
 	{
+		/*gross query; joins Users, User_Scenario_Info, User_Responses, Questions, and Answers to sum up the Score field in Answers for each answer the User selected in U_R.
+		/renames the sum to Total_Score. Most recent limits the results to only their most recent playthroughs. 
+		*/
 		var inQuery = 'SELECT USERID, USERNAME, SUM(SCORE) AS TOTAL_SCORE FROM USERS NATURAL JOIN USER_SCENARIO_INFO NATURAL JOIN USER_RESPONSES NATURAL JOIN QUESTIONS NATURAL JOIN ANSWERS WHERE SCENARIOID = ? AND MOST_RECENT = 1 GROUP BY USERID';
+		
+		//queries the database
 		dbConn.queryDB(mysql.format(inQuery, scenarioid), function(val, err){
 			if(err) {
 				res.send(val);
 			}
 			else {
 				//format accordingly
-				res.send(val);
+				res.send('{ "players:"' + JSON.stringify(val) + "}");
 			}
 		});
 	}
 });
 
+//gets the number of times each answer was selected for each question in a given scenario
 router.get('/scenarioresponses', (req, res) => {
 
+	//gets the scenario parameter
 	var scenarioid = req.query.scenid;
 
+	//makes sure that a parameter was actually set
 	if(scenarioid == undefined)
 	{
 		res.send('ERROR: NO SCENARIOID PARAMETER')
 	}
 	else
 	{
+		//query; joins the tables to count the number of users that selected a specific answer. Only gets the most recent responses
 		var inQuery = 'SELECT SCENARIOID, QUESTIONID, PROMPT, ANSWERID, ANSWER, COUNT(USERID) AS NUMRESPONCES FROM USERS NATURAL JOIN SCENARIOS NATURAL JOIN QUESTIONS NATURAL JOIN ANSWERS NATURAL JOIN USER_RESPONSES NATURAL JOIN USER_SCENARIO_INFO WHERE SCENARIOID = ? AND MOST_RECENT = 1 GROUP BY ANSWERID';
+		
+		//queries db
 		dbConn.queryDB(mysql.format(inQuery, scenarioid), function(val, err){
 			if(err) {
 				res.send(val);
 			}
 			else {
 				//format accordingly
-				res.send(val);
+				res.send(JSON.stringify(val));
 			}
 		});
 	}
 });
 
+//gets each member in a given group
 router.get('/getgroupmembers', (req, res) => {
 
+	//gets a group id.
 	var inserts = req.query.groupid;
-	var inQuery = 'SELECT USERID FROM GROUPS NATURAL JOIN GROUP_MEMBERS WHERE GROUPID = ?';
 
-	dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
-		if(err)
-		{
-			res.send(jSON.stringify(val));
-		}
-		else
-		{
-			res.send(val);
-		}
+	if(inserts == undefined)
+	{
+		res.send('ERR: MISSING PARAMETERS');
+	}
+	else
+	{
+		//query; gets the userid from each user in a given group with the stated groupid
+		var inQuery = 'SELECT USERID FROM GROUPS NATURAL JOIN GROUP_MEMBERS WHERE GROUPID = ?';
 
-	});
-
+		dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
+			
+			if(err) {
+				res.send(JSON.stringify(val));
+			}
+			else {
+				res.send(JSON.stringify(val));
+			}
+		});
+	}
 })
 
-router.get('/getgroupdata', (req, res) => {
+//gets the number of times each answer was selected in a given group
+router.get('/groupresponses', (req, res) => {
 
+	//gets both the scenario's id as well as the group we're searching for
 	var inserts = [req.query.scenid, req.query.groupid];
+	//query, see line 261 for breakdown. Same thing but now adds groups
 	var inQuery = 'SELECT SCENARIOID, QUESTIONID, PROMPT, ANSWERID, ANSWER, COUNT(USERID) AS NUMRESPONCES FROM USERS NATURAL JOIN SCENARIOS NATURAL JOIN QUESTIONS NATURAL JOIN ANSWERS NATURAL JOIN USER_RESPONSES NATURAL JOIN USER_SCENARIO_INFO NATURAL JOIN GROUPS WHERE SCENARIOID = ? AND MOST_RECENT = 1 AND GROUPID = ? GROUP BY ANSWERID'
 
+	//verifies that both parameters were passed in
 	if(inserts[0] == undefined || inserts[1] == undefined)
 	{
 		res.send('ERROR: MISSING PARAMETERS');
 	}
 	else
 	{
+		//queries the db
 		dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
 			if(err) {
 				res.send(val);
 			}
 			else {
 				//format accordingly
-				res.send(val);
+				res.send(JSON.stringify(val));
 			}
 		});
 	}
 });
 
+//gets the scores for members in a specific group in a specific scenario
 router.get('/groupscores', (req, res) => {
 
+	//gets the params
 	var inserts = [req.query.scenid, req.query.groupid];
 
+	//checks to make sure all of the params were sent
 	if(inserts[0] == undefined || inserts[1] == undefined)
 	{
 		res.send('ERROR: NO SCENARIOID PARAMETER')
 	}
 	else
 	{
+		//query; see line 230 for full details. Adds group functionality.
 		var inQuery = 'SELECT USERID, SCENARIOID, SUM(SCORE) AS USER_SCORE, MAX_SCORE FROM USERS NATURAL JOIN USER_SCENARIO_INFO NATURAL JOIN USER_RESPONSES NATURAL JOIN SCENARIOS NATURAL JOIN QUESTIONS NATURAL JOIN ANSWERS NATURAL JOIN GROUP_MEMBERS WHERE SCENARIOID = ? AND GROUPID = ? AND MOST_RECENT = 1 GROUP BY USERID';
+		
+		//queries db
 		dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
 			if(err) {
 				res.send(val);
 			}
 			else {
 				//format accordingly
-				res.send(val);
+				res.send(JSON.stringify(val));
 			}
 		});
 	}
@@ -332,7 +373,7 @@ router.post('/insertCulture', (req, res) => {
 	else
 	{
 		//get query and combine the two
-		var inQuery = "INSERT INTO CULTURES VALUES('0',?,?);";
+		var inQuery = "INSERT INTO CULTURES VALUES('0',?,?)";
 
 		//insert the values into the db. 
 		dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
@@ -347,21 +388,31 @@ router.post('/insertCulture', (req, res) => {
 	}
 });
 
+//inserts a user into the db; expects all info included
 router.post('/insertUser', (req, res) => {
 
 	//get inserts
 	var inserts = [req.body.USERNAME, req.body.FIRSTNAME, req.body.LASTNAME, req.body.EMAIL, 
-				   req.body.PASSWORD, req.body.PROFILE_PIC, req.body.BIRTHDAY, '2017-07-19', '0'];
+				   req.body.PASSWORD, req.body.PROFILE_PIC, req.body.BIRTHDAY, NOW(), '0'];
 
-	if(inserts[0] == undefined || inserts[1] == undefined || inserts[2] == undefined || inserts[3] == undefined || inserts[4] == undefined || 
-	   inserts[5] == undefined || inserts[6] == undefined || inserts[7] == undefined || inserts[8] == undefined)
+	//checks to verify that all necessary fields are filled.
+	if(inserts[0] == undefined || inserts[4] == undefined)
 	{
 		res.send('ERROR: MISSING PARAMETER');
 	}
 	else
 	{
+		var i = 0; 
+		for(i = 0; i < inserts.length; i++)
+		{
+			if(inserts[i] = undefined)
+			{
+				inserts[i] = null;
+			}
+		}
+
 		//get query and combine the two
-		var inQuery = "INSERT INTO USERS VALUES('0',?,?,?,?,?,?,?,?,?);";
+		var inQuery = "INSERT INTO USERS VALUES('0',?,?,?,?,?,?,?,?,?)";
 
 		//insert the values into the db. 
 		dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
@@ -376,15 +427,19 @@ router.post('/insertUser', (req, res) => {
 	}
 });
 
+//sends the player's data from the game to the db
 router.post('/playerdata', (req, res) => {
+	//gets params
 	var inserts = [req.body.USERID, req.body.SCENARIOID, req.body.TIME_COMPLETE, req.body.TIME_PLAYED, req.body.ANSWERS];
 
+	//verifies that all params were passed
 	if(inserts[0] == undefined || inserts[1] == undefined || inserts[2] == undefined || inserts[3] == undefined || inserts[4] == undefined)
 	{
 		console.log('ERROR: MISSING PARAMETER');
 	}
 	else
 	{
+		//passes the data to the db to parse and insert accordingly
 		dbConn.insertUserResponseDB(inserts, function(val, err){
 			if(err) {
 				res.send(val);
@@ -400,13 +455,18 @@ router.post('/playerdata', (req, res) => {
 	
 });
 
+//inserts a scenario into the database given a json file
 router.get('/insertscenario', (req, res) => {
 
 	var inData = testjson;
+
+	//first need to get the cultureid
 	var inQuery = 'SELECT CULTUREID FROM CULTURES WHERE LOCATION = ?';
 
+	//query to get the cultureid using the location from the database
 	dbConn.queryDB(mysql.format(inQuery, inData.LOCATION), function(val, err){
 		console.log(val[0].CULTUREID);
+		//if we found the culture, pass it along with the json to insert the rest of the info
 		if(val[0].CULTUREID != undefined)
 		{
 			dbConn.insertScenarioInfo(inData, val[0].CULTUREID, function(val, err){
@@ -416,11 +476,14 @@ router.get('/insertscenario', (req, res) => {
 	});
 });
 
+//creates a group
 router.post('/creategroup', (req, res) => {
 
+	//gets the params
 	var inserts = [req.body.GROUP_NAME, req.body.CREATOR];
 	var inQuery;
 
+	//can't make a group without a creator userid, but if there's no group name we can proceed with the default "newgroup1"
 	if(req.body.CREATOR == undefined)
 	{
 		res.send('ERR: MISSING PARAMS');
@@ -437,6 +500,7 @@ router.post('/creategroup', (req, res) => {
 
 	}
 
+	//formats query and executes it.
 	dbConn.queryDB(mysql.format(inQuery, inserts), function(val, err){
 		if(err) {
 			res.send('ERR: FAILED TO CREATE GROUP');
@@ -447,29 +511,43 @@ router.post('/creategroup', (req, res) => {
 	});
 });
 
+//adds members into a group. can insert however many id's are passed to it
 router.post('/addmembers', (req, res) => {
 
-	var members = JSON.parse(req.body.userids);
+	var members;
+	//get group to insert into
 	var groupid = req.body.groupid;
-	
-	var inserts = [];
-	var i = 0;
-	for(i = 0; i < members.length; i++)
+
+	if(groupid == undefined || req.body.userid == undefined)
 	{
-		inserts.push([groupid, members[i]]);
+		res.send('ERR: MISSING PARAMS');
 	}
+	else
+	{
+		//parses the userids into a useable array
+		members = JSON.parse(req.body.userids);
 
-	var inQuery = "INSERT INTO GROUP_MEMBERS(GROUPID, USERID) VALUES ?";
-
-	dbConn.queryMulti(inQuery, inserts, function(val, err){
-		if(err) {
-			res.send('ERR: FAILED TO ADD MEMBERS');
+		//prepares to insert each userid by creating an array with arrays containing the insert values
+		var inserts = [];
+		var i = 0;
+		for(i = 0; i < members.length; i++)
+		{
+			inserts.push([groupid, members[i]]);
 		}
-		else {
-			res.send('success');
-		}	
 
-	});
+		//query
+		var inQuery = "INSERT INTO GROUP_MEMBERS(GROUPID, USERID) VALUES ?";
+
+		//inserts each row into the db.
+		dbConn.queryMulti(inQuery, inserts, function(val, err){
+			if(err) {
+				res.send('ERR: FAILED TO ADD MEMBERS');
+			}
+			else {
+				res.send('success');
+			}	
+		});
+	}
 });
 
 
--- server/routes/dbConn.js
+++ server/routes/dbConn.js
@@ -9,7 +9,7 @@ var pool = mysql.createPool({
 	database: "cvrfinal", 
 })
 
-
+//base query function
 exports.queryDB = function queryDB(inQuery,  callback)
 {
 	//connects user to the pool
@@ -34,14 +34,12 @@ exports.queryDB = function queryDB(inQuery,  callback)
 					//if something was returned, return int via callback, otherwise give a no matches found err
 					if(rows.length != 0)
 					{	
-						callback(rows , null);
+						callback(rows, null);
 					}
 					else
 					{
 						callback("ERROR: NO MATCHES FOUND" , "err");
-					}
-					
-										
+					}					
 				}
 				else
 				{
@@ -53,6 +51,7 @@ exports.queryDB = function queryDB(inQuery,  callback)
 	});
 }
 
+//same as queryDB but does multiple queries at once
 exports.queryMulti = function queryMulti(inQuery, multiArr, callback)
 {
 	//connects user to the pool
@@ -82,9 +81,7 @@ exports.queryMulti = function queryMulti(inQuery, multiArr, callback)
 					else
 					{
 						callback("ERROR: NO MATCHES FOUND" , "err");
-					}
-					
-										
+					}			
 				}
 				else
 				{
@@ -97,12 +94,15 @@ exports.queryMulti = function queryMulti(inQuery, multiArr, callback)
 
 }
 
+//inserts data from the game into the db
 exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 {
+	//get initial query
 	var inQuery = "INSERT INTO USER_SCENARIO_INFO VALUES(?,?,?,?,'0')";
 
 	var localInserts = [inserts[0], inserts[1], inserts[2], inserts[3]];
 
+	//connect to db
 	pool.getConnection(function(err, connection){	
 
 		if(err)
@@ -112,9 +112,12 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 		}
 		else
 		{
-			console.log('QUERY: ' + mysql.format(inQuery, localInserts));
+			//console.log('QUERY: ' + mysql.format(inQuery, localInserts));
+
+			//tries to insert the user's initial record into the db
 			connection.query(mysql.format(inQuery, localInserts), function(err, rows, fields){
 				
+				//if we succeeded, insert each response into User_Responses
 				if(!err)
 				{	
 					inQuery = "INSERT INTO USER_RESPONSES (USERID, SCENARIOID, TIME_COMPLETE, QUESTIONID, ANSWERID) VALUES ?";
@@ -122,26 +125,36 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 					var answers = JSON.parse(inserts[4]);
 					var i = 0;
 
+					//creates an array of arrays to insert each row into the db in one call. 
 					for(i = 0; i < answers.length; i++)
 					{
 						values.push([inserts[0], inserts[1], inserts[2], pool.escape(i+1), pool.escape(answers[i])]);
 					}
 
+					//tries to insert the responses into the array
 					connection.query(inQuery, [values], function(err, rows, fields){
 			
+						//if we succeeded, update the most recent fields
 						if(!err)
 						{
+							//get the userid and scenarioid
 							localInserts = [inserts[0], inserts[1]];
+							//query; increments the most recent field by 1 for all rows with the specified userid and scenarioid
 							inQuery = "UPDATE USER_SCENARIO_INFO SET MOST_RECENT = MOST_RECENT + 1 WHERE USERID = ? AND SCENARIOID = ?";
 
+							//tries to update the rows
 							connection.query(mysql.format(inQuery, localInserts), function(err, rows, fields){
 			
+								//if we succeeded, delete any rows with a most recent field greater than 9
 								if(!err)
 								{
+									//prepare query. uses localInserts from the update
 									inQuery = "DELETE FROM USER_SCENARIO_INFO WHERE USERID = ? AND SCENARIOID = ? AND MOST_RECENT > 9";
 
+									//tries to delete the rows
 									connection.query(mysql.format(inQuery, localInserts), function(err, rows, fields){
-
+										
+										//since it's the last query release the connection
 										connection.release();
 			
 										if(!err)
@@ -150,7 +163,7 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 										}
 										else
 										{
-											console.log('ERR: PROBLEM WITH DELETE');
+											//console.log('ERR: PROBLEM WITH DELETE');
 											callback("ERR: PROBLEM WITH DELETE", 'err');
 										}
 										
@@ -159,7 +172,7 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 								else
 								{
 									connection.release();
-									console.log('ERR: PROBLEM WITH UPDATE');
+									//console.log('ERR: PROBLEM WITH UPDATE');
 									callback("ERR: PROBLEM WITH UPDATE", 'err');
 								}
 								
@@ -169,7 +182,7 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 						else
 						{
 							connection.release();
-							console.log('ERR: PROBLEM WITH INSERTING RESPONSES ' + inQuery + ' ' + values);
+							//console.log('ERR: PROBLEM WITH INSERTING RESPONSES ' + inQuery + ' ' + values);
 							callback("ERR: PROBLEM WITH INSERTING RESPONSES " + inQuery + ' ' +  values, 'err');
 							
 						}
@@ -180,7 +193,7 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 				else
 				{
 					connection.release();
-					console.log('ERR: PROBLEM WITH INSERTING INSTANCE');
+					//console.log('ERR: PROBLEM WITH INSERTING INSTANCE');
 					callback("ERR: PROBLEM WITH INSERTING INSTANCE: " + inserts, 'err');
 				}
 				
@@ -193,15 +206,21 @@ exports.insertUserResponseDB = function insertUserResponseDB(inserts, callback)
 
 }
 
+//inserts a scenario into the database using a JSON
 exports.insertScenarioInfo = function insertScenarioInfo(json, cultureid, callback)
 {
+	//get first query initalized
 	var inQuery = "INSERT INTO SCENARIOS VALUES('0',?,?,?,?,?)"
+	//parepare the inserts
 	var inserts = [pool.escape(cultureid), json.TITLE, json.DATE_CREATED, json.SUMMARY, json.MAX_SCORE];
+	//for mass insertion
 	var goals = [], availLang = [], questions = [], answers = [];
 	var scenid;
 
+	//looping through questions + answers
 	var i = 0, j = 0;
 
+	//connect to the db
 	pool.getConnection(function(err, connection){	
 
 		if(err)
@@ -218,23 +237,31 @@ exports.insertScenarioInfo = function insertScenarioInfo(json, cultureid, callba
 				//if there wasn't an error, handle the result, otherwise something happened with connecting to db
 				if(!err)
 				{
+					//tries to get the scenarioid after we insert it based on the cultureid and title (combined are unique)
 					inQuery = 'SELECT SCENARIOID FROM SCENARIOS WHERE CULTUREID = ? AND TITLE = ?';
 					inserts = [pool.escape(cultureid), json.TITLE];
+
+					//tries to get the id
 					connection.query(mysql.format(inQuery, inserts), function(err, rows, fields){
 					
+						//if we succeed try to insert the goals into the GOALS table
 						if(!err)
 						{
 							scenid = rows[0].SCENARIOID;
 							
+							//prepares mass insertion by building array of arrays with info
 							for(i = 0; i < json.GOALS.length; i++)
 							{
 								goals.push([pool.escape(scenid), json.GOALS[i].GOAL]);
 							}
 
+							//prepares query
 							inQuery = 'INSERT INTO GOALS (SCENARIOID, GOAL) VALUES ?'
 
+							//inserts goals into the db
 							connection.query(inQuery, [goals], function(err, rows, fields){
 
+								//if we succeed repeate the process for available languages
 								if(!err)
 								{
 									for(i = 0; i < json.AVAILABLE_LANGUANGES.length; i++)
@@ -244,30 +271,44 @@ exports.insertScenarioInfo = function insertScenarioInfo(json, cultureid, callba
 
 									inQuery = 'INSERT INTO LANGUAGES (SCENARIOID, AVAIL_LANGUAGE) VALUES ?';
 								
+									//try to insert the available languages into the db
 									connection.query(inQuery, [availLang], function(err, rows, fields){
 
+										//if we succeeded, try to insert the questions and answers
 										if(!err)
 										{
+											//get the relative indexes for the questions and answers. 
 											for(i = 0; i < json.QUESTIONS.length; i++)
 											{
+												//builds the question inserts, initializing the id's to i+1
 												questions.push([pool.escape(i+1), pool.escape(scenid), json.QUESTIONS[i].PROMPT]);
+												//loops through each answer for the question i
 												for(j = 0; j < json.QUESTIONS[i].ANSWERS.length; j++)
 												{
+													//builds the answers inserts, initializing the questions id to i+1 and answers id to j+1
 													answers.push([pool.escape(j+1), pool.escape(i+1), pool.escape(scenid), json.QUESTIONS[i].ANSWERS[j].ANSWER, json.QUESTIONS[i].ANSWERS[j].SCORE, json.QUESTIONS[i].ANSWERS[j].REASONING]);
 												}
 											}
 
+											//query for inserting questions
 											inQuery = 'INSERT INTO QUESTIONS (QUESTIONID, SCENARIOID, PROMPT) VALUES ?';
 
+											//tries to mass insert questions
 											connection.query(inQuery, [questions], function(err, rows, fields){
 
+												//if we succeed, try to finish it with inserting answers
 												if(!err)
 												{
+													//get the answers query
 													inQuery = 'INSERT INTO ANSWERS (ANSWERID, QUESTIONID, SCENARIOID, ANSWER, SCORE, REASONING) VALUES ?';
+													
+													//tries to mass insert answers into the db
 													connection.query(inQuery, [answers], function(err, rows, fields){
 
+														//release the connection since we're done
 														connection.release();
 
+														//return status
 														if(!err)
 														{
 															callback('successfully added everything', null);
@@ -314,9 +355,6 @@ exports.insertScenarioInfo = function insertScenarioInfo(json, cultureid, callba
 			});
 		}	
 	});
-
-
-
 }
 
 //	var userQuery = "SELECT USERID, DATE_JOINED, TIME_PLAYED, PROFILE_PIC, EMAIL_ADDR, SCENARIOID, TITLE,  TIME_PLAYED, QUESTIONID, PROMPT, ANSWERID, ANSWER" + 
@@ -334,11 +372,14 @@ exports.formatUserJSON = function formatUserJSON(str, callback)
 	var totalTime = 0;
 	var i = 0;
 
+	//loop through each row recieved from the db
 	for(i = 0; i < str.length; i++)
 	{
 		
+		//if the userid is still the current one, continue with that user's data
 		if(currID == str[i].USERID)
 		{
+			//if the scenairoid and timestamp are the same, then add the new question + answer record
 			if(currScenID == str[i].SCENARIOID && currTimeComplete == str[i].TIME_COMPLETE)
 			{
 				retString += ',{"QUESTIONID": "' + str[i].QUESTIONID  + '",';
@@ -347,6 +388,7 @@ exports.formatUserJSON = function formatUserJSON(str, callback)
 				retString += '"ANSWER": "' + str[i].ANSWER + '"}';
 
 			}
+			//otherwise we're either in a new scenario or different playthrough. End the previous scenario and create a new one
 			else
 			{
 				retString += ']},{"SCENARIOID": "' + str[i].SCENARIOID  + '",';
@@ -363,6 +405,7 @@ exports.formatUserJSON = function formatUserJSON(str, callback)
 				currTimeComplete = str[i].TIME_COMPLETE;
 			}
 		}
+		//otherwise add a new user (always goes here when i = 0)
 		else
 		{
 			if(i != 0)
@@ -386,6 +429,7 @@ exports.formatUserJSON = function formatUserJSON(str, callback)
 			retString += '"ANSWERID": "' + str[i].ANSWERID + '",';
 			retString += '"ANSWER": "' + str[i].ANSWER + '"}';
 			
+			//sets current data
 			currID = str[i].USERID;
 			currScenID = str[i].SCENARIOID;
 			currTimeComplete = str[i].TIME_COMPLETE;
@@ -396,6 +440,7 @@ exports.formatUserJSON = function formatUserJSON(str, callback)
 
 	}
 
+	//finishes the JSON
 	retString += ']]}]}]}';
 
 	callback(retString, null);
